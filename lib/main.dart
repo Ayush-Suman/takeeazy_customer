14b5a69e1479fb91c0969ecbd572650193a77e0c
Addepalli N M PavanKalyan <57014239+pavankalyan0424@users.noreply.github.com>
2021-01-01 14:09:13 +0530

    resolved conflicts

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,9 @@ import 'package:takeeazy_customer/model/dialog/dialogmanager.dart';
 import 'package:takeeazy_customer/screens/bottomnav/bottonnav.dart';
 import 'package:takeeazy_customer/screens/map/locationselect.dart';
 
-
-
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  NetworkCalls.initialise(tokenModifier: (t)=>"Bearer $t");
+  NetworkCalls.initialise(tokenModifier: (t) => "Bearer $t");
   runApp(MyApp());
 }
 
@@ -20,17 +18,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: (context, widget)=> Navigator(
-        onGenerateRoute: (settings)=> MaterialPageRoute(builder: (context) => DialogManager(child: widget)),
+      builder: (context, widget) => Navigator(
+        onGenerateRoute: (settings) => MaterialPageRoute(
+            builder: (context) => DialogManager(child: widget)),
       ),
       title: 'TakeEazy',
       theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          bottomSheetTheme: BottomSheetThemeData(modalBackgroundColor: Colors.transparent),
-      appBarTheme: AppBarTheme( iconTheme: IconThemeData(
-        color: Color(0xff3B6E9E),
-      ),color: Colors.white,elevation: 0,),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        bottomSheetTheme:
+            BottomSheetThemeData(modalBackgroundColor: Colors.transparent),
+        appBarTheme: AppBarTheme(
+          centerTitle: true,
+          textTheme: TextTheme(
+            headline6: TextStyle(
+              color: Color(0xff3B6E9E),
+              fontFamily: 'Rubik',
+              fontWeight: FontWeight.w400,
+              fontSize: 18.96,
+              fontStyle: FontStyle.normal,
+            ),
+          ),
+          iconTheme: IconThemeData(
+            color: Color(0xff3B6E9E),
+          ),
+          color: Colors.white,
+          elevation: 0,
+        ),
       ),
       initialRoute: TERoutes.home,
       onGenerateRoute: TERoutes.generateRoutes,
@@ -39,37 +53,38 @@ class MyApp extends StatelessWidget {
 }
 
 class TERoutes {
-
   static const home = '/';
   static const map = 'map';
   static final locationController = LocationController();
   static final homeController = HomeController();
 
-  static Route<dynamic> generateRoutes(RouteSettings settings){
-    switch(settings.name){
+  static Route<dynamic> generateRoutes(RouteSettings settings) {
+    switch (settings.name) {
       case home:
-
         return MaterialPageRoute(
-          builder: (_) =>
-              MultiProvider(providers: [ChangeNotifierProvider.value(value:homeController
+          builder: (_) => MultiProvider(
+            providers: [
+              ChangeNotifierProvider.value(value: homeController),
+              ChangeNotifierProvider.value(
+                  value: homeController.serviceableAreaController)
+            ],
+            builder: (_, a) => BottomNav(),
           ),
-          ChangeNotifierProvider.value(value: homeController.serviceableAreaController)],
-                builder: (_, a)=> BottomNav(),
-              ),
-
         );
 
       case map:
         return MaterialPageRoute(
-          builder: (_)=> ChangeNotifierProvider.value(
-              value: locationController,
-              builder:(_, a)=> LocationSelect()
-          )
-        );
+            builder: (_) => ChangeNotifierProvider.value(
+                value: locationController,
+                builder: (_, a) => LocationSelect()));
 
-      default: return MaterialPageRoute(
-          builder: (_) => Scaffold(body: Center(child: Text("No such route as ${settings.name}"),),));
+      default:
+        return MaterialPageRoute(
+            builder: (_) => Scaffold(
+                  body: Center(
+                    child: Text("No such route as ${settings.name}"),
+                  ),
+                ));
     }
   }
-
 }
