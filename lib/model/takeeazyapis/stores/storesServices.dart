47804031c3a10cd8a78f21293afcd922cf68b71b
Ayush Suman <ayush231suman@gmail.com>
2021-01-07 17:54:16 +0530

    Minor Error Fixes

--- lib/model/takeeazyapis/stores/storesServices.dart
+++ lib/model/takeeazyapis/stores/storesServices.dart
@@ -1,31 +1,31 @@
-import 'package:takeeazy_customer/model/base/URLRoutes.dart';
+import 'package:takeeazy_customer/model/takeeazyapis/base/URLRoutes.dart';
 import 'package:takeeazy_customer/model/base/calltype.dart';
 import 'package:takeeazy_customer/model/base/networkcall.dart';
-import 'package:takeeazy_customer/model/stores/storesModel.dart';
+import 'package:takeeazy_customer/model/takeeazyapis/stores/storesModel.dart';
 
 class StoresServices {
-  static Future<ShopModel> getStoresByDistance(
+  static Future<TEResponse> getStoresByDistance(
       {double latitude = 0.0, double longitude = 0.0, double dist = 0}) async {
     Map<String, String> param = {
       'dist': dist.toString(),
       'geo': latitude.toString() + ',' + longitude.toString()
     };
-    ShopModel shopModel = await request<ShopModel>(URLRoutes.getShops,
+    TEResponse<ShopModel> shopModel = await request<ShopModel>(URLRoutes.getShops,
         call: CALLTYPE.GET, param: param, auth: true);
     return shopModel;
   }
 
-  static Future<ShopModel> getStoresByContainer(
+  static Future<TEResponse> getStoresByContainer(
       {String container = 'Groceries %26 Essentials'}) async {
     Map<String, String> param = {
       'container': container,
     };
-    ShopModel shopModel = await request<ShopModel>(URLRoutes.getShops,
+    TEResponse<ShopModel> shopModel = await request<ShopModel>(URLRoutes.getShops,
         call: CALLTYPE.GET, param: param, auth: true);
     return shopModel;
   }
 
-  static Future<ShopModel> getStoresByDistanceInAContainer(
+  static Future<TEResponse> getStoresByDistanceInAContainer(
       {String container = 'Groceries %26 Essentials',
       double latitude = 0.0,
       double longitude = 0.0,
@@ -35,7 +35,7 @@ class StoresServices {
       'geo': latitude.toString() + ',' + longitude.toString(),
       'container': container
     };
-    ShopModel shopModel = await request<ShopModel>(URLRoutes.getShops,
+    TEResponse<ShopModel> shopModel = await request<ShopModel>(URLRoutes.getShops,
         call: CALLTYPE.GET, param: param, auth: true);
     return shopModel;
   }
