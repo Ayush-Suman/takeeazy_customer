2d2bf0e20f00525aa4e597ca0778fb66a2dd195a
Ayush Suman <ayush231suman@gmail.com>
2021-01-29 01:41:57 +0530

    Merging

--- lib/screens/cart/cart.dart
+++ lib/screens/cart/cart.dart
@@ -23,6 +23,20 @@ class Cart extends StatelessWidget {
           fontWeight: FontWeight.w700,
         ),
       ),
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: TEButton(
+              height: 50,
+              width: width,
+              text: TEText(
+                  text: "ORDER NOW",
+                  fontWeight: FontWeight.w700,
+                  fontColor: Color(0xffffffff)),
+              onPressed: () async {
+                cartController.orderNow();
+              }),
+        ),
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: ChangeNotifierProvider.value(value: cartController.cartListController, builder: (_,a)=>Consumer<CartListController>(builder: (_, cc, child)=>cc.updatedController.value?cc.list.length!=0?ListView.builder(
         padding: const EdgeInsets.all(8),
         itemBuilder: (context, index) => CartCard(cc.list[index], cartController.quantities[index]),
